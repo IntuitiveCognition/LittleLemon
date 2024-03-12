0d5e00fecb1d79c2245fc3388976689fe5c6d35a
cognitiveintuition <charom20@hotmail.com>
2024-03-12 06:50:08 -0400

    table booking API

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -12,3 +12,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = "__all__"
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,17 @@
 #define URL route for index() view
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+
+router: DefaultRouter = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking/', include(router.urls)),
 ]
