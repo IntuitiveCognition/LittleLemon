db837202a45051a96d8607db57843d10cefca733
cognitiveintuition <charom20@hotmail.com>
2024-03-13 08:56:18 -0400

    add API docs

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -144,6 +144,8 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
-        #"rest_framework_xml.renderers.XMLRenderer",
+        
     ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+
 }
--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-
+from rest_framework.documentation import include_docs_urls
 
 
 
@@ -13,5 +13,5 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('api-token-auth/', obtain_auth_token),
-    
+    path('docs/', include_docs_urls(title='Little Lemon API')),
 ]
