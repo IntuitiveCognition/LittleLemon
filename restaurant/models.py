2366fcd8ad6c3bfdaff87e7ccebf8d02358a6f33
cognitiveintuition <charom20@hotmail.com>
2024-03-13 05:40:26 -0400

    testing api

--- restaurant/models.py
+++ restaurant/models.py
@@ -18,4 +18,4 @@ class Menu(models.Model):
     Inventory = models.PositiveSmallIntegerField()
 
     def __str__(self):
-        return self.Title
+        return f'{self.title} : {str(self.price)}'
